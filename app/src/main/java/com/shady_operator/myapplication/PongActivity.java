06a4eabd42a7a1ba276b0bed83690d61bebee0b4
KFitz114 <keegan.fitzgerald@yahoo.com>
2022-08-28 21:05:35 -0400

    Added the Display class and mPongGame class.

--- app/src/main/java/com/shady_operator/myapplication/PongActivity.java
+++ app/src/main/java/com/shady_operator/myapplication/PongActivity.java
@@ -5,7 +5,12 @@ import android.view.Window;
 import android.os.Bundle;
 import android.view.WindowManager;
 
+import android.graphics.Point;
+import android.view.Display;
+
 public class PongActivity extends Activity {
+
+    private PongGame mPongGame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +18,12 @@ public class PongActivity extends Activity {
 
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+
+        mPongGame = new PongGame(this, size.x, size.y);
+        setContentView(mPongGame);
     }
 }
